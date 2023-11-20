8196a535db3258d94b61963295a5ff51d7712864
Косенко Роман Дмитриевич <roman.kosenko@megamarket.ru>
2023-11-20 22:49:06 +0300

    closure solution done

--- 01-exercise/01-goroutines/06-closure/main.go
+++ 01-exercise/01-goroutines/06-closure/main.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	// what is the output
-	//TODO: fix the issue.
-
+	// what is the output 4 4 4
+	// for now output is 3 2 1
 	for i := 1; i <= 3; i++ {
 		wg.Add(1)
-		go func() {
+		go func(v int) {
 			defer wg.Done()
-			fmt.Println(i)
-		}()
+			fmt.Println(v)
+		}(i)
 	}
+
 	wg.Wait()
 }
